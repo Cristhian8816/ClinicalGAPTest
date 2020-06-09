2d801efa34f103997182f3713c6b6183801b38bc
Cristhian8816 <cristhian.julian@hotmail.com>
2020-06-08 20:13:45 -0500

    Adding some unitests to get API mehotds

---
+++ backend_APINet/ClinicalAPI/ClinicalAPITestProject1/GetMethodTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ClinicalAPI.Controllers;
+using ClinicalAPI.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Http;
+
+namespace ClinicalAPITestProject1
+{
+    [TestClass]
+    public class GetMethodTests
+    {
+        [TestMethod]
+        public void TestGetpatient()
+        {
+            //Arrange
+            var patientID = 1;
+            PatientsController patientsController = new PatientsController();
+
+            //Act 
+            IHttpActionResult actionResult = patientsController.Getpatient(patientID);            
+
+            //Assert
+            Assert.IsNotNull(actionResult);                 
+        }
+
+        public void TestGetappointment()
+        {
+            //Arrange
+            var appintmentID = 1;
+            AppointmentsController appointmentController = new AppointmentsController();
+
+            //Act 
+            IHttpActionResult actionResult = appointmentController.Getappointmentt(appintmentID);
+
+            //Assert
+            Assert.IsNotNull(actionResult);
+        }
+    }
+}
---
+++ backend_APINet/ClinicalAPI/ClinicalAPITestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClinicalAPITestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClinicalAPITestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6feeab46-2a01-4594-a2d5-cf4471070524")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
